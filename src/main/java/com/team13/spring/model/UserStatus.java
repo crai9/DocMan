7613c2446b0c9ded39e6b56f3e15b1ec34dbdd02
Craig Miller <craigm1357@gmail.com>
2015-03-01 02:55:37 +0000

    Implemented base REST controller, will allow client-side username validation

---
+++ src/main/java/com/team13/spring/model/UserStatus.java
@@ -0,0 +1,17 @@
+package com.team13.spring.model;
+
+public class UserStatus {
+
+	private Boolean available;
+
+	public Boolean getAvailable() {
+		return available;
+	}
+
+	public void setAvailable(Boolean available) {
+		this.available = available;
+	}
+	
+	
+	
+}
